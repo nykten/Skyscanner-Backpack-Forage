30a3de4968a3f97b78e43e0b2e46eaa510355d76
Nik Harith <2673209b@student.gla.ac.uk>
2025-08-07 11:41:14 +0100

    modified test file to pass it by mocking

--- backpacking-forage/src/App.test.js
+++ backpacking-forage/src/App.test.js
@@ -1,6 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('@skyscanner/backpack-web/bpk-component-calendar', () => {
+  return {
+    __esModule: true,
+    default: () => <div>MockCalendar</div>,
+    CALENDAR_SELECTION_TYPE: {
+      single: 'single',
+      range: 'range',
+    },
+  };
+});
+jest.mock('@skyscanner/backpack-web/bpk-component-input', () => {
+  return {
+    __esModule: true,
+    default: () => <input />,
+    INPUT_TYPES: {
+      text: 'text',
+    },
+  };
+});
+
+jest.mock('date-fns/format', () => () => 'mocked-date');
+
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
