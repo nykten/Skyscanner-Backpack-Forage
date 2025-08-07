2ef399586b5c2af98cd0b7d0aba922bae1d76a6e
Nik Harith <2673209b@student.gla.ac.uk>
2025-08-07 09:49:17 +0100

    Added calendar component to App.js, but react uncompatible

--- backpacking-forage/src/App.js
+++ backpacking-forage/src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="Flight Schedule">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+import BpkCalendar, {
+  CALENDAR_SELECTION_TYPE,
+} from '@skyscanner/backpack-web/bpk-component-calendar';
+import BpkInput, {
+  INPUT_TYPES,
+} from '@skyscanner/backpack-web/bpk-component-input';
+import format from 'date-fns/format';
+
+const formatDateFull = (date) => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = (date) => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr:'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+];
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="Flight Schedule">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+      },
+    };
+  }
+
+  handleDateSelect = (date) => {
+  this.setState({
+    selectionConfiguration: {
+      type: this.props.selectionConfiguration.type,
+      date: date,
+    },
+  });
+};
+
+  render() {
+    return (
+      <div>
+        <BpkInput
+          id="dateInput"
+          type={INPUT_TYPES.text}
+          name="date"
+          value={(this.state.selectionConfiguration.date || '').toString()}
+          placeholder="Departure date"
+        />
+        <BpkCalendar
+          id="calendar"
+          onDateSelect={this.handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel={"Change month"}
+          nextMonthLabel={"Next month"}
+          previousMonthLabel={"Previous month"}
+          selectionConfiguration={this.state.selectionConfiguration}
+        />
+      </div>
+    );
+  }
 }
 
-export default App;
