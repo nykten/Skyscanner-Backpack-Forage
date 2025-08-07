dd505130991aa5512c4f5747fee291843547099d
Nik Harith <2673209b@student.gla.ac.uk>
2025-08-07 10:05:11 +0100

    Cleaned up App.js

--- backpacking-forage/src/App.js
+++ backpacking-forage/src/App.js
@@ -67,6 +67,23 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <div className="App">
+          <header className="Flight Schedule">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+
         <BpkInput
           id="dateInput"
           type={INPUT_TYPES.text}
